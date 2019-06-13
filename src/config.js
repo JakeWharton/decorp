57919a5d5cd4d778993852e70f03c55fb126c0b1
Jake Wharton <jakew@google.com>
2019-06-13 14:41:11 -0400

    Track config.js since we moved to env vars for config

---
+++ src/config.js
@@ -0,0 +1,12 @@
+var config = {
+  slack_api_token: process.env.DECORP_SLACK_API_TOKEN,
+
+  regexes: [
+    { regex: /https:\/\/b\.corp\.google\.com\/issues\/(\d+)#([^ >]+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]#[2]' },
+    { regex: /https:\/\/b\.corp\.google\.com\/issues\/(\d+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]' }
+  ],
+
+  schedules: [],
+  build: function (id) {}
+};
+module.exports = config;
