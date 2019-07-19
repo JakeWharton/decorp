06c48b46778ee2668c7f1fa2b2291118c32101aa
cketti <cketti@gmail.com>
2019-07-19 20:35:21 +0200

    Add support for partnerissuetracker links

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ var config = {
   slack_api_token: process.env.DECORP_SLACK_API_TOKEN,
 
   regexes: [
-    { regex: /https:\/\/b\.corp\.google\.com\/issues\/(\d+)#([^ >]+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]#[2]' },
-    { regex: /https:\/\/b\.corp\.google\.com\/issues\/(\d+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]' }
+    { regex: /https:\/\/(?:b|partnerissuetracker)\.corp\.google\.com\/issues\/(\d+)#([^ >]+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]#[2]' },
+    { regex: /https:\/\/(?:b|partnerissuetracker)\.corp\.google\.com\/issues\/(\d+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]' }
   ]
 };
 module.exports = config;
