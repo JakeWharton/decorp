e7131408b0ac0e05f3e33a8ca7462bb433c6a18f
Jake Wharton <jakew@google.com>
2019-07-26 09:45:18 -0400

    Add support for b/ short links

--- src/config.js
+++ src/config.js
@@ -2,10 +2,11 @@ var config = {
   slack_api_token: process.env.DECORP_SLACK_API_TOKEN,
 
   regexes: [
-    { regex: /https:\/\/b\.corp\.google\.com\/issues\/(\d+)#([^ >]+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]#[2]' },
-    { regex: /https:\/\/b\.corp\.google\.com\/issues\/(\d+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]' },
-    { regex: /https:\/\/buganizer\.corp\.google\.com\/issues\/(\d+)#([^ >]+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]#[2]' },
-    { regex: /https:\/\/buganizer\.corp\.google\.com\/issues\/(\d+)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]' }
+    { regex: /(?: |^)https:\/\/b\.corp\.google\.com\/issues\/(\d+)#([^ >]+)(?: |$)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]#[2]' },
+    { regex: /(?: |^)https:\/\/b\.corp\.google\.com\/issues\/(\d+)(?: |$)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]' },
+    { regex: /(?: |^)https:\/\/buganizer\.corp\.google\.com\/issues\/(\d+)#([^ >]+)(?: |$)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]#[2]' },
+    { regex: /(?: |^)https:\/\/buganizer\.corp\.google\.com\/issues\/(\d+)(?: |$)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]' },
+    { regex: /(?: |^)b\/(\d+)(?: |$)/g, message: 'Public link: https://issuetracker.google.com/issues/[1]' }
   ]
 };
 module.exports = config;
--- test/test.js
+++ test/test.js
@@ -8,10 +8,20 @@ var randomiser = function (max) { return rnd; };
 regexbot = new RegexBot(config, randomiser);
 
 describe('Regexbot', function () {
-  it('should handle basic b.corp link', function () {
+  it('should handle b.corp link', function () {
     var reply = '';
 
+    regexbot.respond('https://b.corp.google.com/issues/135190668', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668');
+
     regexbot.respond('check out https://b.corp.google.com/issues/135190668', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668');
+
+    regexbot.respond('https://b.corp.google.com/issues/135190668 blah blah', function (txt) {
       reply = txt;
     });
     assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668');
@@ -19,7 +29,18 @@ describe('Regexbot', function () {
 
   it('should handle b.corp links with URL fragments', function () {
     var reply = '';
+
+    regexbot.respond('https://b.corp.google.com/issues/135190668#comment1', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668#comment1');
+
     regexbot.respond('check out https://b.corp.google.com/issues/135190668#comment1', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668#comment1');
+
+    regexbot.respond('https://b.corp.google.com/issues/135190668#comment1 blah blah', function (txt) {
       reply = txt;
     });
     assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668#comment1');
@@ -28,7 +49,17 @@ describe('Regexbot', function () {
   it('should handle buganizer.corp link', function () {
     var reply = '';
 
+    regexbot.respond('https://buganizer.corp.google.com/issues/135190668', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668');
+
     regexbot.respond('check out https://buganizer.corp.google.com/issues/135190668', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668');
+
+    regexbot.respond('https://buganizer.corp.google.com/issues/135190668 blah blah', function (txt) {
       reply = txt;
     });
     assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668');
@@ -36,10 +67,54 @@ describe('Regexbot', function () {
 
   it('should handle buganizer.corp links with URL fragments', function () {
     var reply = '';
+
+    regexbot.respond('https://buganizer.corp.google.com/issues/135190668#comment1', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668#comment1');
+
     regexbot.respond('check out https://buganizer.corp.google.com/issues/135190668#comment1', function (txt) {
       reply = txt;
     });
     assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668#comment1');
+
+    regexbot.respond('https://buganizer.corp.google.com/issues/135190668#comment1 blah blah', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668#comment1');
+  });
+
+  it('should handle b/ link', function () {
+    var reply = '';
+
+    regexbot.respond('b/135190668', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668');
+
+    regexbot.respond('check out b/135190668', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668');
+
+    regexbot.respond('b/135190668 blah', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, 'Public link: https://issuetracker.google.com/issues/135190668');
+  });
+
+  it('should ignore b/ link inside word', function () {
+    var reply = '';
+
+    regexbot.respond('foo.com/b/135190668', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, '');
+
+    regexbot.respond('b/135190668/b', function (txt) {
+      reply = txt;
+    });
+    assert.equal(reply, '');
   });
 
   it('should handle multiple links', function () {
